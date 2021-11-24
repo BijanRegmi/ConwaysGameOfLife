00b4d7c5ed5b7c56e657e4f2e0732291869961cc
Bijan Regmi <55084653+BijanRegmi@users.noreply.github.com>
2021-11-24 17:54:34 +0545

    Buttons, dk if this works or not

---
+++ include/Button.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+class Button {
+public:
+    Button(){}
+	
+	void Init(std::string str, sf::Vector2f size, int charSize, uint32_t color, sf::Font& font);
+	void setPosition(sf::Vector2f point);
+
+	void draw(sf::RenderWindow& window);
+
+	bool isMouseOver(sf::RenderWindow& window);
+	bool isClicked(sf::RenderWindow& window);
+
+    void SetOnClick(std::function<void()> func);
+private:
+	sf::RectangleShape button;
+	sf::Text text;
+    std::function<void()> onClickFxn;
+};
