bbbd2fc93661c8b2e9b14f0d0ba68bc518949fbc
Bijan Regmi <vizandabijan@gmail.com>
2021-11-24 17:54:34 +0545

    Buttons, dk if this works or not

---
+++ src/Button.cpp
@@ -0,0 +1,54 @@
+#include "Button.hpp"
+
+void Button::Init(std::string str, sf::Vector2f size, int charSize, uint32_t color, sf::Font& font) {
+    button.setSize(size);
+    button.setFillColor(sf::Color(color));
+    button.setOutlineColor(sf::Color(0x002461ff));
+
+    // Change origin for box
+    sf::FloatRect rect = button.getLocalBounds();
+    button.setOrigin(rect.left+rect.width/2, rect.top+rect.height/2);
+
+    text.setString(str);
+    text.setFont(font);
+    text.setCharacterSize(charSize);
+
+    // Change origin for text
+    sf::FloatRect trect = text.getLocalBounds();
+    text.setOrigin(trect.left+trect.width/2, trect.top+trect.height/2);
+}
+
+void Button::setPosition(sf::Vector2f point) {
+    button.setPosition(point);
+    text.setPosition(point);
+}
+
+void Button::draw(sf::RenderWindow &window) {
+    window.draw(button);
+    window.draw(text);
+}
+
+void Button::SetOnClick(std::function<void()> func){
+    onClickFxn = func;
+}
+
+bool Button::isMouseOver(sf::RenderWindow& window) {
+    sf::FloatRect rect = button.getGlobalBounds();
+    sf::Vector2i mPos = sf::Mouse::getPosition(window);
+    if (rect.contains(mPos.x, mPos.y)){
+        button.setOutlineThickness(1);
+        return true;
+    } else {
+        button.setOutlineThickness(0);
+        return false;
+    }
+}
+
+bool Button::isClicked(sf::RenderWindow& window){
+    if (isMouseOver(window) && sf::Mouse::isButtonPressed(sf::Mouse::Left)){
+        onClickFxn();
+        return true;
+    } else {
+        return false;
+    }
+}
